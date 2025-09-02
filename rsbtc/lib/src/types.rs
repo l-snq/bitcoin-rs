21a71f24eb17e76a4f509d5a143473cd12efda06
snackkk <tastytothesupreme@gmail.com>
2025-09-01 18:41:24 -0600

    added transaction template, and filled out the blockheaders. Created stub methods for each struct

--- rsbtc/lib/src/types.rs
+++ rsbtc/lib/src/types.rs
@@ -1,4 +1,5 @@
 use crate::U256;
+use uuid::Uuid;
 
 pub struct Blockchain {
     pub blocks: Vec<Block>,
@@ -10,14 +11,33 @@ pub struct Block {
 }
 
 pub struct BlockHeader {
+    /// Timestamp of the block
     pub timestamp: u64,
+    /// Nonce used to mine the block
     pub nonce: u64,
+    /// Hash of the previous block
     pub prev_block_hash: [u8; 32],
+    /// Merkle root of the block's transactions
     pub merkle_root: [u8; 32],
+    /// target
     pub target: U256,
 }
 
-pub struct Transaction;
+pub struct Transaction {
+    pub inputs: Vec<TransactionInput>,
+    pub outputs: Vec<TransactionOutput>,
+}
+
+pub struct TransactionInput {
+    pub prev_transaction_output_hash: [u8; 32],
+    pub signature: [u8; 64],
+}
+
+pub struct TransactionOutput {
+    pub value: u64,
+    pub unique_id: Uuid,
+    pub pubkey: [u8; 33],
+}
 
 impl Blockchain {
     pub fn new() -> Self {
@@ -42,5 +62,40 @@ impl Block {
 
     pub fn hash(&self) -> ! {
         unimplemented!()
+    }
+}
+
+impl BlockHeader {
+    pub fn new(
+        timestamp: u64,
+        nonce: u64,
+        prev_block_hash: [u8; 32],
+        merkle_root: [u8; 32],
+        target: U256,
+    ) -> Self {
+        BlockHeader {
+            timestamp,
+            nonce,
+            prev_block_hash,
+            merkle_root,
+            target,
+        }
+    }
+
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+
+    }
+}
+
+impl Transaction {
+    pub fn new(
+        inputs: Vec<TransactionInput>,
+        outputs: Vec<TransactionOutput>,
+    ) -> Self {
+        Transaction {
+            inputs: inputs,
+            outputs: outputs,
+        }
     }
 }
